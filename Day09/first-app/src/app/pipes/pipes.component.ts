361283e6623db9117ef596cf49d32839eed4f095
Giovanni Ordonez <giovanniordonez33@gmail.com>
2022-07-25 00:25:31 -0500

    pagination

--- Day09/first-app/src/app/pipes/pipes.component.ts
+++ Day09/first-app/src/app/pipes/pipes.component.ts
@@ -15,11 +15,49 @@ export class PipesComponent{
 
     product = { id : 100, name : 'Pen', cost : 10}
 
-    products : string[] = [];
+    products : Array<string> = Array();
 
     constructor(){
         for (let i = 1; i <= 100; i++)
             this.products.push(`Product - ${i}`)
     }
+    //This constructor will create an object with the 100 objects that I will
+    //need to paginate through
+
+    start: number = 0;
+    end: number = 0;
+    pageBy: string = '5';
+    currentPage: number = 0;
+    totalPages: number = this.products.length / parseInt(this.pageBy);
+
+    goToBeginning(){
+        this.start = 0;
+        this.end = parseInt(this.pageBy);
+    }
+
+    goToEnd(){
+        this.start = this.products.length - parseInt(this.pageBy);
+        this.end = this.products.length;
+        this.currentPage = this.totalPages-1;
+    }
+
+    goBack(){
+        if(this.start !== 0){
+            this.start = this.start - parseInt(this.pageBy)
+            this.end = this.end - parseInt(this.pageBy)
+            this.currentPage = this.currentPage-1;
+            console.log(parseInt(this.pageBy))
+            console.log(this.products.length / parseInt(this.pageBy))
+        }
+    }
+
+    goForwards(){
+        if (this.end != this.products.length){
+            this.start = this.end;
+            this.end = this.end + parseInt(this.pageBy);
+            this.currentPage =  this.currentPage + 1;
+        }
+    }
+
     
 }
