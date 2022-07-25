c330f82d7989f4e3dc163ec9546b69ef7a1ff41f
Giovanni Ordonez <giovanniordonez33@gmail.com>
2022-07-24 20:30:07 -0500

    PipesAssignment

--- Day09/first-app/src/app/pipes/pipes.component.ts
+++ Day09/first-app/src/app/pipes/pipes.component.ts
@@ -12,4 +12,14 @@ export class PipesComponent{
     //date pipe
     date: string = '';
     currentDate: Date = new Date();
+
+    product = { id : 100, name : 'Pen', cost : 10}
+
+    products : string[] = [];
+
+    constructor(){
+        for (let i = 1; i <= 100; i++)
+            this.products.push(`Product - ${i}`)
+    }
+    
 }
